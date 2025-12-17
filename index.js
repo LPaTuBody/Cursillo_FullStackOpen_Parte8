a5ce779e47d2ad1dc82ab132f78630889376d522
LPaTuBody <erodriguezrr@cincinnatus.edu.do>
2025-12-17 11:23:44 -0400

    Ejercicio 8.2
    Agregando la funcionalidad que devuelva todos los libros

--- index.js
+++ index.js
@@ -90,9 +90,18 @@ let books = [
 */
 
 const typeDefs = `
+  type Books {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Books!]!
   }
 `
 
@@ -100,6 +109,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   }
 }
 
