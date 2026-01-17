495d06e5060c949c60105d0418be1de976125bc2
LPaTuBody <erodriguezrr@cincinnatus.edu.do>
2026-01-17 19:58:46 -0400

    Ejercicio 8.19
    Creando filtro en la lista de libros por género.

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,16 +1,47 @@
+import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client/react";
 import { ALL_BOOKS } from "../queries";
 
 const Books = ({ show }) => {
   if (!show) return null;
+
+  const [genres, setGenres] = useState(["all genres"]);
+  const [filtGenre, setFiltGenre] = useState("all genres");
+  const [filtBooks, setFiltBooks] = useState([]);
   const result = useQuery(ALL_BOOKS);
+  
+  useEffect(() => {
+    if(!result.loading) setFiltBooks(result.data.allBooks);
+  }, [result.loading]);
 
   if (result.loading) return (<div>Loading...</div>);
+  
   const books = result.data.allBooks;
+  
+  books.forEach(b => {
+    for (let i in b.genres) {
+      const genero = b.genres[i];
+      if (!genres.includes(genero)) setGenres(genres.concat(genero));
+    }
+  }); // array de los géneros para generar los botones
+
+  const handleFilter = (genero) => {
+    setFiltGenre(genero);
+    if (genero === "all genres") setFiltBooks(books);
+    else setFiltBooks(books.filter(b => (b.genres.includes(genero))));
+  }
 
   return (
     <div>
       <h2>Books</h2>
+      <div>
+        <div>
+          {genres.map(g => (
+            <button key={g} onClick={() => handleFilter(g)}>{g}</button>
+          ))}
+        </div>
+        <p><strong>In genre:</strong> {filtGenre}</p>
+      </div>
       <table>
         <tbody>
           <tr>
@@ -18,7 +49,7 @@ const Books = ({ show }) => {
             <th>Author</th>
             <th>Published</th>
           </tr>
-          {books.map((b) => (
+          {filtBooks.map((b) => (
             <tr key={b.id}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
